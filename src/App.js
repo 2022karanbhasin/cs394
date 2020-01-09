8180fb6b8b0b4be4d2483a40513a7752ad95701e
Karan Bhasin <karanbhasin2022@u.northwestern.edu>
2020-01-09 00:26:46 -0600

    slice 6

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 
 
 
 const App = () => {
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(addScheduleTimes(json));
-      }
-    fetchSchedule();
-  }, [])
+    const handleData = snap => {
+      if (snap.val()) setSchedule(addScheduleTimes(snap.val()));
+    }
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+  }, []);
 
   return (
     <Container>
       <Banner title={ schedule.title } />
       <CourseList courses={ schedule.courses } />
     </Container>
-  );
+);
 };
 
 const days = ['M', 'Tu', 'W', 'Th', 'F'];
@@ -109,7 +109,7 @@ const addCourseTimes = course => ({
 
 const addScheduleTimes = schedule => ({
   title: schedule.title,
-  courses: schedule.courses.map(addCourseTimes)
+  courses: Object.values(schedule.courses).map(addCourseTimes)
 });
 
 const daysOverlap = (days1, days2) => ( 
@@ -138,11 +138,41 @@ const hasConflict = (course, selected) => (
 
 const Course = ({ course, state }) => (
   <Button color={ buttonColor(state.selected.includes(course)) }
-    onClick={ () => state.toggle(course) }
-    disabled={ hasConflict(course, state.selected) }
-    >
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </Button>
-);
+      onClick={ () => state.toggle(course) }
+      onDoubleClick={ () => moveCourse(course) }
+      disabled={ hasConflict(course, state.selected) }
+      >
+      { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+    </Button>
+  );
+
+  const moveCourse = course => {
+    const meets = prompt('Enter new meeting data, in this format:', course.meets);
+    if (!meets) return;
+    const {days} = timeParts(meets);
+    if (days) saveCourse(course, meets); 
+    else moveCourse(course);
+  };
+  
+  const saveCourse = (course, meets) => {
+    db.child('courses').child(course.id).update({meets})
+      .catch(error => alert(error));
+  };
 
 export default App;
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWgcXoklaphdspYZB4UYdy3NKvZKOdLuE",
+  authDomain: "cs394hw1.firebaseapp.com",
+  databaseURL: "https://cs394hw1.firebaseio.com",
+  projectId: "cs394hw1",
+  storageBucket: "cs394hw1.appspot.com",
+  messagingSenderId: "754382513047",
+  appId: "1:754382513047:web:193e8b9f59f9ddea111ad2",
+  measurementId: "G-T8SL7B5ZNZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
